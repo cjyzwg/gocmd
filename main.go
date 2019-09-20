a306a9a39a8d9fd832d84e2959bd610eb12d2940
cjyzwg <1044035278@qq.com>
2019-09-20 17:30:09 +0800

    adad

--- main.go
+++ main.go
@@ -12,56 +12,7 @@ func main() {
 	app.Name = "cjcmd"
 	app.Usage = "cjcmd工具集"
 	app.Version = Version
-	app.Commands = []cli.Command{
-		{
-			Name:    "new",
-			Aliases: []string{"n"},
-			Usage:   "create new project",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:        "o",
-					Value:       "",
-					Usage:       "project owner for create project",
-					Destination: &p.Owner,
-				},
-				cli.StringFlag{
-					Name:        "d",
-					Value:       "",
-					Usage:       "project directory for create project",
-					Destination: &p.Path,
-				},
-				cli.BoolFlag{
-					Name:        "proto",
-					Usage:       "whether to use protobuf for create project",
-					Destination: &p.WithGRPC,
-				},
-				cli.StringFlag{
-					Name:        "m",
-					Usage:       "project module name for create project, for `go mod init`",
-					Destination: &p.ModuleName,
-				},
-			},
-			Action: runNew,
-		},
-		{
-			Name:    "build",
-			Aliases: []string{"b"},
-			Usage:   "cjcmd build",
-			Action:  buildAction,
-		},
-		{
-			Name:    "run",
-			Aliases: []string{"r"},
-			Usage:   "cjcmd run",
-			Action:  runAction,
-		},
-		{
-			Name:            "tool",
-			Aliases:         []string{"t"},
-			Usage:           "cjcmd tool",
-			Action:          toolAction,
-			SkipFlagParsing: true,
-		},
+	app.Commands = []cli.Command{		
 		{
 			Name:    "version",
 			Aliases: []string{"v"},
@@ -70,12 +21,7 @@ func main() {
 				fmt.Println(getVersion())
 				return nil
 			},
-		},
-		{
-			Name:   "self-upgrade",
-			Usage:  "cjcmd self-upgrade",
-			Action: upgradeAction,
-		},
+		}
 	}
 	err := app.Run(os.Args)
 	if err != nil {
